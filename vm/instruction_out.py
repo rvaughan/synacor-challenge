94a8df22dd5327cf78f763418d15b631f7c6c7ee
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-12-28 09:25:31 +0000

    Clean up the decoded output a little bit.

--- vm/instruction_out.py
+++ vm/instruction_out.py
@@ -8,11 +8,24 @@ class Out_Instruction(Instruction):
     def __init__(self, location):
         self.location = location
 
+    def _get_char(self, vm_state):
+        result = None
+
+        if self.location < 256:
+            result = self.location
+        else:
+            result = self._get_value(vm_state, self.location)
+
+        if result == 10:
+            return '\\n'
+        else:
+            try:
+                return chr(result)
+            except ValueError:
+                return '*'
+
     def dump(self, vm_state):
-        print "[{:04X}] OUT {:0}".format(vm_state["instruction_pointer"]-2, self.location)
+        print "[{:04X}] OUT {:0} '{}'".format(vm_state["instruction_pointer"]-2, self.location, self._get_char(vm_state))
 
     def execute(self, vm_state):
-        if self.location < 256:
-            sys.stdout.write(chr(self.location))
-        else:
-            sys.stdout.write(chr(self._get_value(vm_state, self.location)))
+        sys.stdout.write(self._get_char(vm_state))
