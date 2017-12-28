361eb3e094b56039f0414b559c539961edacccb1
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-12-28 06:43:01 +0000

    Fixes for pop instruction.

--- vm/instruction_pop.py
+++ vm/instruction_pop.py
@@ -6,12 +6,11 @@ from instruction import Instruction
 class Pop_Instruction(Instruction):
 
     def __init__(self, location):
-        logging.debug("Creating POP")
-
         self.location = location
 
     def dump(self, vm_state):
         print "[{:04X}] POP {:0}".format(vm_state["instruction_pointer"]-2, self.location)
 
     def execute(self, vm_state):
-        logging.debug("Executing POP")
+        value = vm_state["stack"].pop()
+        self._set_value(vm_state, self.location, value)
