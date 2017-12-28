153f24d7db9e10e6af91768923842265e81650b0
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-12-28 07:13:01 +0000

    Fixes for rmem and wmem instructions.

--- vm/instruction_rmem.py
+++ vm/instruction_rmem.py
@@ -6,8 +6,6 @@ from instruction import Instruction
 class RMem_Instruction(Instruction):
 
     def __init__(self, location, value):
-        logging.debug("Creating RMEM")
-
         self.location = location
         self.value = value
 
@@ -15,4 +13,6 @@ class RMem_Instruction(Instruction):
         print "[{:04X}] RMEM {:0} {:0}".format(vm_state["instruction_pointer"]-3, self.location, self.value)
 
     def execute(self, vm_state):
-        logging.debug("Executing RMEM")
+        mem_location = self._get_value(vm_state, self.value)
+
+        self._set_value(vm_state, self.location, vm_state["memory"][mem_location])
--- vm/instruction_wmem.py
+++ vm/instruction_wmem.py
@@ -6,8 +6,6 @@ from instruction import Instruction
 class WMem_Instruction(Instruction):
 
     def __init__(self, location, value):
-        logging.debug("Creating WMEM")
-
         self.location = location
         self.value = value
 
@@ -15,4 +13,6 @@ class WMem_Instruction(Instruction):
         print "[{:04X}] WMEM {:0} {:0}".format(vm_state["instruction_pointer"]-3, self.location, self.value)
 
     def execute(self, vm_state):
-        logging.debug("Executing WMEM")
+        mem_loc = self._get_value(vm_state, self.location)
+        value = self._get_value(vm_state, self.value)
+        vm_state["memory"][mem_loc] = value
