f55c01217a90c5331ca936b00dbff0ed30c9fb2c
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-12-28 06:43:10 +0000

    Fixes for push instruction.

--- vm/instruction_push.py
+++ vm/instruction_push.py
@@ -6,12 +6,11 @@ from instruction import Instruction
 class Push_Instruction(Instruction):
 
     def __init__(self, value):
-        logging.debug("Creating PSH")
-
         self.value = value
 
     def dump(self, vm_state):
         print "[{:04X}] PSH {:0}".format(vm_state["instruction_pointer"]-2, self.value)
 
     def execute(self, vm_state):
-        logging.debug("Executing PSH")
+        value = self._get_value(vm_state, self.value)
+        vm_state["stack"].append(value)
