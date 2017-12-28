494ba99c7ef95c0264ec9b64bfde2e8cd6ae53d1
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-12-28 06:42:43 +0000

    Fixes for not instruction.

--- vm/instruction_not.py
+++ vm/instruction_not.py
@@ -6,8 +6,6 @@ from instruction import Instruction
 class Not_Instruction(Instruction):
 
     def __init__(self, location, value):
-        logging.debug("Creating NOT")
-
         self.location = location
         self.value = value
 
@@ -15,4 +13,8 @@ class Not_Instruction(Instruction):
         print "[{:04X}] NOT {:0} {:0}".format(vm_state["instruction_pointer"]-3, self.location, self.value)
 
     def execute(self, vm_state):
-        logging.debug("Executing NOT")
+        value = self._get_value(vm_state, self.value)
+
+        # Need to remember that the values stored must be 15 bit only, so
+        # we need to mask off the 16th bit...
+        self._set_value(vm_state, self.location, (~value & ((1 << 15) - 1)))
