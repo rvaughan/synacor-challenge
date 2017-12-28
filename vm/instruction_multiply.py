265bba099d3541f382edc96b36af30b2290236fe
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-12-28 06:57:22 +0000

    Fixes for mult instruction.

--- vm/instruction_multiply.py
+++ vm/instruction_multiply.py
@@ -6,8 +6,6 @@ from instruction import Instruction
 class Multiply_Instruction(Instruction):
 
     def __init__(self, location, value_a, value_b):
-        logging.debug("Creating MULT")
-
         self.location = location
         self.value_a = value_a
         self.value_b = value_b
@@ -16,4 +14,9 @@ class Multiply_Instruction(Instruction):
         print "[{:04X}] MUL {:0} {:0} {:0}".format(vm_state["instruction_pointer"]-4, self.location, self.value_a, self.value_b)
 
     def execute(self, vm_state):
-        logging.debug("Executing MULT")
+        val_a = self._get_value(vm_state, self.value_a)
+        val_b = self._get_value(vm_state, self.value_b)
+
+        result = (val_a * val_b) % 32768
+
+        self._set_value(vm_state, self.location, result)
