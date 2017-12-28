b9688eed2d2eca93f86496791b21b0d7c520f7b1
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-12-28 06:43:18 +0000

    Fixes for set instruction.

--- vm/instruction_set.py
+++ vm/instruction_set.py
@@ -6,8 +6,6 @@ from instruction import Instruction
 class Set_Instruction(Instruction):
 
     def __init__(self, register, value):
-        logging.debug("Creating SET")
-
         self.register = register
         self.value = value
 
@@ -15,4 +13,4 @@ class Set_Instruction(Instruction):
         print "[{:04X}] SET {:0} {:0}".format(vm_state["instruction_pointer"]-3, self.register, self.value)
 
     def execute(self, vm_state):
-        logging.debug("Executing SET")
+        self._set_value(vm_state, self.register, self.value)
