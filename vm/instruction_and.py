af69eab114fd91f0852146b9f9bf5daa07d4d020
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-12-28 06:42:08 +0000

    Fixes for AND instruction.

--- vm/instruction_and.py
+++ vm/instruction_and.py
@@ -6,7 +6,6 @@ from instruction import Instruction
 class And_Instruction(Instruction):
 
     def __init__(self, location, value_a, value_b):
-        logging.debug("Creating AND")
         self.location = location
         self.value_a = value_a
         self.value_b = value_b
@@ -15,4 +14,7 @@ class And_Instruction(Instruction):
         print "[{:04X}] AND {:0} {:0} {:0}".format(vm_state["instruction_pointer"]-4, self.location, self.value_a, self.value_b)
 
     def execute(self, vm_state):
-        logging.debug("Executing AND")
+        value_a = self._get_value(vm_state, self.value_a)
+        value_b = self._get_value(vm_state, self.value_b)
+
+        self._set_value(vm_state, self.location, (value_a & value_b))
