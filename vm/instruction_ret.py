e7419b662d53c4426b9584b0760bc49057c25ce6
Richard Vaughan <richard.vaughan@xuvasi.com>
2017-12-28 07:16:09 +0000

    Fixes for pop instruction.

--- vm/instruction_ret.py
+++ vm/instruction_ret.py
@@ -5,12 +5,11 @@ from instruction import Instruction
 
 class Ret_Instruction(Instruction):
 
-    def __init__(self, location):
-        logging.debug("Creating RET")
-        self.location = location
+    def __init__(self):
+        pass
 
     def dump(self, vm_state):
         print "[{:04X}] RET".format(vm_state["instruction_pointer"]-2)
 
     def execute(self, vm_state):
-        logging.debug("Executing RET")
+        vm_state["instruction_pointer"] = vm_state["stack"].pop()
